46cb459efc6b6f2fef677ababc78bc317815bbc3
Rivyan <59740343+Rivyan@users.noreply.github.com>
2020-10-27 10:07:06 +0000

    change footer styling

--- pages/en/moments/about/index.jsx
+++ pages/en/moments/about/index.jsx
@@ -10,15 +10,16 @@ export default function About() {
       <Container fluid className={styles.aboutContainer}>
         <Row>
           <Col>
-            <p className={styles.aboutTitle}>
-              "Azért fényképezünk, mert nem tudunk beletörődni, hogy minden
-              elmúlik, hogy lehetetlen megismételni egy már átélt pillanatot.
-              Egyhangú háborút vívunk saját halálunk fenyegető réme ellen, és az
-              idő ellen, amely másfajta és kevésbé értékes lényekké: felnőttekké
-              változtatja a gyereket. Azért fényképezünk, mert tudjuk magunkról,
-              hogy felejtünk. Elfelejtjük a hetet, a napot, az órát; elfelejtjük
-              azt a pillanatot, amikor a legboldogabbak voltunk." (Michelle
-              Richmond)
+            <p className={styles.aboutQuote}>
+              “We take pictures because we can't accept that everything passes,
+              we can't accept that the repetition of a moment is an
+              impossibility. We wage a monotonous war against our own impending
+              deaths, against time that turns children into that other, lesser
+              species: adults. We take pictures because we know we will forget.
+              We will forget the week, the day, the hour. We will forget when we
+              were happiest. We take pictures out of pride, a desire to have the
+              best of ourselve preserved. We fear that we will die and others
+              will not know we lived.”
             </p>
           </Col>
         </Row>
@@ -32,25 +33,38 @@ export default function About() {
           </Col>
           <Col>
             <div className={styles.aboutText}>
-              <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h1>
+              <h1>
+                Hello there, my name is Viktoria Markus and I am a family,
+                children and women photographer.
+              </h1>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-                lobortis eleifend pellentesque. Proin efficitur sed tellus vel
-                rhoncus. Pellentesque non massa eget enim semper iaculis quis
-                hendrerit magna. Duis eget libero ligula. Sed viverra euismod
-                ante eu maximus. In non ornare tortor, et iaculis erat. Ut justo
-                elit, accumsan id lectus et, feugiat congue massa. Fusce
-                imperdiet fringilla rhoncus. Quisque non tellus non turpis
-                pharetra pretium. Nulla hendrerit pellentesque lorem et auctor.
-                Ut fermentum varius interdum. Nulla elementum tincidunt mauris,
-                sagittis venenatis quam bibendum vitae. Ut consectetur vehicula
-                sem ac molestie. Nam dapibus, ligula id molestie aliquet, mi
-                eros egestas magna, a mattis nulla dui eu neque. Donec sit amet
-                ipsum eleifend diam feugiat elementum. Suspendisse nec
-                vestibulum quam, sed imperdiet dolor. Suspendisse eget
-                scelerisque massa. Donec scelerisque orci nec porttitor
-                bibendum. Sed ex ex, elementum sed ornare eget, fringilla ut
-                nunc.
+                Family and children photography came to my life naturally. After
+                my daughters were born, I wanted to take better and better
+                photos of them. For this reason, I bought a reflex camera and
+                started taking classes and doing workshops. Each followed
+                another, and soon I got my photographer certificate. At this
+                point, I took photos of everybody in my vicinity: family,
+                friends, even in my kids kindergarten. Nowadays I usually give
+                photos to people as gifts, since the best way to see time flying
+                by is to watch our children and the only way to stop it for a
+                second is to take a quick snap of them. This way, I can give
+                eternal memories to grandparents, families, since it will be
+                tremendous joy to take out these photos in the future to come.
+                Taking photos of children and families became one of my
+                favourite things to do. But in the meantime, I found another
+                challenge: immortalising the beauty of women, and soon it became
+                my other one. Just for a few hour, anybody can take a break from
+                their jobs or their families, but all of these women agree in
+                one thing: it was an adventure and an unbelievable experience. A
+                few hours, which was just about them. Watching them as they see
+                themselves in professional makeup for the first time, looking at
+                their faces when they take a glimpse on the screen of the photos
+                we made together, not believing that they are the ones on these
+                images… Just pure bliss, every time, giving me goosebumps. Every
+                time they told me that the photo session was the equivalent of a
+                therapy. And it has to be the case, since I have the same
+                feeling each and every time… Come, and lets eternalise a few
+                memories together!
               </p>
             </div>
           </Col>
