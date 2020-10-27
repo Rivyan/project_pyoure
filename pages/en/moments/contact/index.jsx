d1ed9587fa7eed584faab213d96dccd1051530a9
Rivyan <59740343+Rivyan@users.noreply.github.com>
2020-10-27 10:24:33 +0000

    make en/moments/contact responsive

--- pages/en/moments/contact/index.jsx
+++ pages/en/moments/contact/index.jsx
@@ -9,16 +9,16 @@ export default function Contact() {
       <Navigation />
       <Container className={styles.contactContainer}>
         <Row>
-          <Col>
+          <Col className={styles.contactInfo} xs={12} md={6}>
             <h1>HOW TO CONTACT ME</h1>
             <p>
-              Please uf the contact form to email me directly with any queries.
+              Please use the contact form to email me directly with any queries.
             </p>
             <p>If you prefer to phone, call me on + 36 30 1234567</p>
           </Col>
-          <Col>
+          <Col className={styles.contactForm} xs={12} md={6}>
             <Form>
-              <Form.Group controlId="exampleForm.ControlInput1">
+              <Form.Group>
                 <Form.Label>Email address</Form.Label>
                 <Form.Control type="email" placeholder="name@example.com" />
               </Form.Group>
@@ -30,9 +30,9 @@ export default function Contact() {
                 <Form.Label>Last Name</Form.Label>
                 <Form.Control as="textarea" rows="1" />
               </Form.Group>
-              <Form.Group controlId="exampleForm.ControlTextarea1">
+              <Form.Group>
                 <Form.Label>Message</Form.Label>
-                <Form.Control as="textarea" rows="3" />
+                <Form.Control as="textarea" rows="8" />
               </Form.Group>
               <Button variant="primary" type="submit">
                 Submit
